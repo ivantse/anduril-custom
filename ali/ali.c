bac2100367d03e124722279068dd7ef4f5824598
Ivan Tse <github@nerdtower.com>
2022-01-31 12:04:41 -0800

    ali config

---
+++ ali/ali.c
@@ -0,0 +1,3 @@
+#undef EV_click10_hold
+#define EV_click10_hold EV_none
+#undef USE_BATTCHECK
---
+++ ali/cfg-noctigon-kr4-nofet.h
@@ -0,0 +1,12 @@
+// Noctigon KR4 (fetless) config options for Anduril
+// (and Noctigon KR1)
+// (and Emisar D4v2 E21A, a.k.a. "D4v2.5")
+#include "../../bzr/anduril2/ToyKeeper/spaghetti-monster/anduril/cfg-noctigon-kr4.h"
+
+// **** Ali Customizations ****
+#undef USE_LOCKOUT_MODE
+#undef USE_AUTOLOCK
+#undef RGB_LED_OFF_DEFAULT
+#define RGB_LED_OFF_DEFAULT 0x15 // purple
+
+#define OVERRIDES_FILE ali.c
