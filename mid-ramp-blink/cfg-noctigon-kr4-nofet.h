7c1b1329eaf855e310a9630c688b0a0b829e416f
Ivan Tse <github@nerdtower.com>
2022-01-31 15:08:45 -0800

    mid-ramp-blink

---
+++ mid-ramp-blink/cfg-noctigon-kr4-nofet.h
@@ -0,0 +1,7 @@
+// Noctigon KR4 (fetless) config options for Anduril
+// (and Noctigon KR1)
+// (and Emisar D4v2 E21A, a.k.a. "D4v2.5")
+#include "../../bzr/anduril2/ToyKeeper/spaghetti-monster/anduril/cfg-noctigon-kr4.h"
+
+#undef BLINK_AT_RAMP_MIDDLE_1
+#define BLINK_AT_RAMP_MIDDLE_1 65
