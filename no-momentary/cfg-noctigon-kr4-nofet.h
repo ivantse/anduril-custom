ed9a422ff5a2360b76fa42fb246959a81c839721
Ivan Tse <github@nerdtower.com>
2022-02-02 11:46:05 -0800

    no-momentary

---
+++ no-momentary/cfg-noctigon-kr4-nofet.h
@@ -0,0 +1,6 @@
+// Noctigon KR4 (fetless) config options for Anduril
+// (and Noctigon KR1)
+// (and Emisar D4v2 E21A, a.k.a. "D4v2.5")
+#include "../../bzr/anduril2/ToyKeeper/spaghetti-monster/anduril/cfg-noctigon-kr4-nofet.h"
+
+#undef USE_MOMENTARY_MODE
